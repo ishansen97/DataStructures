94346110029b7b6d237a63cef93d3469ea55bd5a
Ishan Seneviratne <ishan.s@calcey.com>
2021-10-04 18:12:05 +0530

    Add project files.

---
+++ DataStructures/Program.cs
@@ -0,0 +1,49 @@
+﻿using DataStructures.Stack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackMethod();
+            SomeMethod1();
+        }
+
+        private static void StackMethod()
+        {
+            // create items
+            var item1 = new Item { Name = "Ishan", Value = 100 };
+            var item2 = new Item { Name = "Erandi", Value = 101 };
+            var item3 = new Item { Name = "Pika", Value = 102 };
+            var item4 = new Item { Name = "James", Value = 103 };
+            var item5 = new Item { Name = "Andrew", Value = 104 };
+            // create the stack
+            var stack = new StackTest();
+
+            // push items
+            stack.Push(item1);
+            stack.Push(item2);
+            stack.Push(item3);
+            stack.Push(item4);
+            stack.Push(item5);
+
+            SomeMethod2();
+
+            // peek and see :O
+            Console.WriteLine($"Top item: {stack.Peek().Name}");
+
+            // remove top
+            var topItem = stack.Pop();
+            Console.WriteLine($"Removed Item: {topItem.Name}");
+            Console.WriteLine($"Top after removing: {stack.Peek().Name}");
+        }
+
+        private static void SomeMethod1() => Console.WriteLine("method 1");
+
+        private static void SomeMethod2() => Console.WriteLine("inside method");
+    }
+}
---
+++ DataStructures/Stack/StackTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Stack
+{
+    public class StackTest
+    {
+        public int Top { get; set; }
+
+        public int Size { get; set; } = 5;
+
+        private Item[] Items { get; set; }
+
+        public StackTest()
+        {
+            Top = -1;
+            Items = new Item[Size];
+        }
+
+        #region stack operations
+        public void Push(Item item)
+        {
+            var result = IsFull();
+            if (!IsFull())
+            {
+                Items[++Top] = item;
+                // optional
+                /*Top += 1;
+                Items[Top] = item;*/
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("stack eka pirila yako, enna epa");
+            }
+        }
+
+        public Item Pop()
+        {
+            if (!IsEmpty())
+            {
+                //best
+                return Items[Top--];
+
+                // optional
+                /*var item = Items[Top];
+                Top--;
+                return item;*/
+            }
+            throw new ArgumentNullException("empty yako");
+        }
+
+        public Item Peek()
+        {
+            if (!IsEmpty())
+            {
+                return Items[Top];
+            }
+            throw new ArgumentNullException("empty yako");
+        }
+
+        #endregion
+
+        public bool IsFull() => Top == Size - 1;
+
+        public bool IsEmpty() => Top == -1;
+    }
+}
